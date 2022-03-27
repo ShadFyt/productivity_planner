1908c85ad6cef51b38b01546f91dfa7d287e2ae5
ShadFyt <doanryan10@gmail.com>
2022-03-27 16:21:00 -0500

    added  app to project

--- functional_tests.py
+++ functional_tests.py
@@ -9,9 +9,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
+    # Bob has heard about a new online productivity planner app.
+    # He goes to check out the homepage
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get("http://localhost:8000")
 
+        # He notices the page title is 'Productivity Planner'
         self.assertIn("Productivity Planner", self.browser.title)
         self.fail("Finished the test!")
 
