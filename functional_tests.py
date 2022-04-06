2696787d8d309a3edbd6de36a6b639ca38fa1935
ShadFyt <doanryan10@gmail.com>
2022-04-05 22:43:15 -0500

    added more functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
 import unittest
 
@@ -18,24 +19,27 @@ class NewVisitorTest(unittest.TestCase):
 
         # client notices the page title is 'Productivity Planner'
         self.assertIn("Productivity Planner", self.browser.title)
-        header_text = self.browser.find_element_by_tag_name("h1").text
+        header_text = self.browser.find_element(By.TAG_NAME, value="h1").text
         self.assertIn("Productivity Planner For Software Developers", header_text)
 
         # client is invited to enter a new task
-        inputbox = self.browser.find_element_by_id("new_task")
+        inputbox = self.browser.find_element(By.ID, value="new_task")
         self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a new task")
         # Client enters "Write tests for homepage"
         inputbox.send_keys("Write tests for homepage")
         # When the client hits enter, the page will update, and list
         # "Write tests for homepage"
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        table = self.browser.find_element_by_id("task_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertTrue(
-            any(row.text == "Write tests for homepage" for row in rows),
-            "New task did not appear in table",
-        )
+        # client enters a 2nd task to do
+        inputbox = self.browser.find_element(By.ID, value="new_task")
+
+        inputbox.send_keys("Write view logic for homepage")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(10)
+        table = self.browser.find_element(By.ID, value="task_table")
+        rows = table.find_elements(by=By.TAG_NAME, value="tr")
+        self.assertIn("Write tests for homepage", [row.text for row in rows])
+        self.assertIn("Write view logic for homepage", [row.text for row in rows])
         self.fail("Finished the test!")
 
 
--- planner/tests.py
+++ planner/tests.py
@@ -1,7 +1,13 @@
+from urllib import response
 from django.test import TestCase
 
 
 class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_can_save_POST_request(self):
+        response = self.client.post("/", data={"task_text": "Write tests for homepage"})
+        self.assertIn("Write tests for homepage", response.content.decode())
         self.assertTemplateUsed(response, "home.html")
